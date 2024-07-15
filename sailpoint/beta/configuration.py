295a47414577a8240b262369a488ff269676ee4f
tyler-mairose-sp <tyler-mairose-sp@users.noreply.github.com>
2024-07-15 14:21:04 +0000

    Bump version to 1.0.6

--- sailpoint/beta/configuration.py
+++ sailpoint/beta/configuration.py
@@ -390,7 +390,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 3.1.0-beta\n"\
-               "SDK Package Version: 1.0.5".\
+               "SDK Package Version: 1.0.6".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
