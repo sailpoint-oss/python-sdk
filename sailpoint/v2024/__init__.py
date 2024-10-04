033915365523a7f8af9704e96a6c9f7825b8d4b7
tyler-mairose-sp <tyler-mairose-sp@users.noreply.github.com>
2024-10-04 14:19:07 +0000

    Bump version to 1.1.8

--- sailpoint/v2024/__init__.py
+++ sailpoint/v2024/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.1.7"
+__version__ = "1.1.8"
 
 # import apis into sdk package
 from sailpoint.v2024.api.access_model_metadata_api import AccessModelMetadataApi
--- sailpoint/v3/configuration.py
+++ sailpoint/v3/configuration.py
@@ -406,7 +406,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 3.0.0\n"\
-               "SDK Package Version: 1.1.7".\
+               "SDK Package Version: 1.1.8".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
