e39a08a55a15643d53eb6cd408e7fd36c9dd9a8b
tyler-mairose-sp <tyler-mairose-sp@users.noreply.github.com>
2024-11-04 15:13:48 +0000

    Bump version to 1.1.9

--- sailpoint/v2024/__init__.py
+++ sailpoint/v2024/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.1.8"
+__version__ = "1.1.9"
 
 # import apis into sdk package
 from sailpoint.v2024.api.access_model_metadata_api import AccessModelMetadataApi
