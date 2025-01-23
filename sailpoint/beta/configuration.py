8337c3351992013212bbd961bb906f1487f529a4
darrell-thobe-sp <darrell-thobe-sp@users.noreply.github.com>
2025-01-23 16:55:34 +0000

    Bump version to 1.2.2

--- sailpoint/beta/configuration.py
+++ sailpoint/beta/configuration.py
@@ -406,7 +406,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 3.1.0-beta\n"\
-               "SDK Package Version: 1.2.1".\
+               "SDK Package Version: 1.2.2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
--- sailpoint/v2024/configuration.py
+++ sailpoint/v2024/configuration.py
@@ -406,7 +406,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v2024\n"\
-               "SDK Package Version: 1.2.1".\
+               "SDK Package Version: 1.2.2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
