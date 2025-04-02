3ef81b1a7dda8ea9069e82c0b1a3b23de2766091
tyler-mairose-sp <tyler-mairose-sp@users.noreply.github.com>
2025-04-02 13:16:36 +0000

    Bump version to 1.3.0

--- sailpoint/v2025/configuration.py
+++ sailpoint/v2025/configuration.py
@@ -519,7 +519,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v2025\n"\
-               "SDK Package Version: 1.2.8".\
+               "SDK Package Version: 1.3.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
