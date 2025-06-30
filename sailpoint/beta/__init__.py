59aba1ec23393d7a90ee33349c0f05fddef00f71
darrell-thobe-sp <darrell-thobe-sp@users.noreply.github.com>
2025-06-30 14:04:40 +0000

    Bump version to 1.3.5

--- sailpoint/beta/__init__.py
+++ sailpoint/beta/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.3.4"
+__version__ = "1.3.5"
 
 # import apis into sdk package
 from sailpoint.beta.api.access_model_metadata_api import AccessModelMetadataApi
