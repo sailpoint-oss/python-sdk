6eda4f342e5fa368fb7409fa8577e6d679b72473
BhanuKari <72975678+BhanuKari@users.noreply.github.com>
2025-11-06 23:12:49 +0530

    Cascading the verify_ssl as is to config

--- sailpoint/configuration.py
+++ sailpoint/configuration.py
@@ -44,7 +44,7 @@ class Configuration:
         self.access_token = configurationParams.access_token if configurationParams and configurationParams.access_token else defaultConfiguration.access_token
         self.proxy = configurationParams.proxy if configurationParams and configurationParams.proxy else None
         self.proxy_headers = configurationParams.proxy_headers if configurationParams and configurationParams.proxy_headers else None
-        self.verify_ssl = configurationParams.verify_ssl if configurationParams and configurationParams.verify_ssl else True
+        self.verify_ssl = configurationParams.verify_ssl if configurationParams and not configurationParams.verify_ssl else True
 
         url = f"{self.token_url}"
         if self.access_token == None:
