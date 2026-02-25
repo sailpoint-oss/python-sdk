61ec095a3dcbdb88cac1c04bbcf6fa6867fb7840
luke-hagar-sp <193985956+luke-hagar-sp@users.noreply.github.com>
2026-02-25 08:12:52 +0000

    Bump version to v1.4.16

--- sailpoint/v2026/__init__.py
+++ sailpoint/v2026/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.4.15"
+__version__ = "1.4.16"
 
 # import apis into sdk package
 from sailpoint.v2026.api.account_deletion_requests_api import AccountDeletionRequestsApi
