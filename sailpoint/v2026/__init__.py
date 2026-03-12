f725224542c3f31f3e4021f17ef108438e5ea05a
luke-hagar-sp <193985956+luke-hagar-sp@users.noreply.github.com>
2026-03-12 08:10:38 +0000

    Bump version to v1.4.28

--- sailpoint/v2026/__init__.py
+++ sailpoint/v2026/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.4.27"
+__version__ = "1.4.28"
 
 # import apis into sdk package
 from sailpoint.v2026.api.access_requests_api import AccessRequestsApi
--- sailpoint/v3/__init__.py
+++ sailpoint/v3/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.4.27"
+__version__ = "1.4.28"
 
 # import apis into sdk package
 from sailpoint.v3.api.access_profiles_api import AccessProfilesApi
