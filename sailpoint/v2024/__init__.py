f06336fc1729090ac355206f611787a2addbb872
luke-hagar-sp <193985956+luke-hagar-sp@users.noreply.github.com>
2026-05-13 08:59:16 +0000

    Bump version to v1.4.69

--- sailpoint/v2024/__init__.py
+++ sailpoint/v2024/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.4.68"
+__version__ = "1.4.69"
 
 # import apis into sdk package
 from sailpoint.v2024.api.access_model_metadata_api import AccessModelMetadataApi
